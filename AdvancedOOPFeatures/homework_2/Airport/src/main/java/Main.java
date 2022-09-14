a119c08c70e28fdd07415375eeae86be9bd7cacc
Dmitriy Belkin <dimaalexxxxx13@gmail.com>
2022-09-14 21:05:16 +0300

    commit1

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,38 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+import org.apache.commons.collections.collection.CompositeCollection;
+
+import javax.swing.text.html.Option;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        LocalDateTime time = LocalDateTime.now().plusHours(2);
+        LocalDateTime nowTime = LocalDateTime.now();
+        return airport.getTerminals().stream()
+                .flatMap((terminal -> terminal.getFlights().stream()
+                        .filter(flight -> flight.getDate().toInstant().
+                                atZone(ZoneId.systemDefault()).toLocalDateTime().isBefore(time) &&
+                                flight.getDate().toInstant().
+                                        atZone(ZoneId.systemDefault()).toLocalDateTime().isAfter(nowTime))))
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE)).collect(Collectors.toList());
     }
 
 }
