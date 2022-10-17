2f20827530f89f6fcccb10e72c9540936b4be776
Dmitriy Belkin <dimaalexxxxx13@gmail.com>
2022-10-18 02:55:18 +0300

    commit2

--- Data/src/main/java/Main.java
+++ Data/src/main/java/Main.java
@@ -1,22 +1,34 @@
+
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     private static final Metro metro = new Metro();
-    private static final String LINE_REGEX = "[А-Я-]{3}+[0-9]{1}";
+    private static final String LINE_REGEX = "[А-Я-]{3}+[0-9]{1}$";
+    private static final String file = "Data/data";
+    private static String path = "";
+    private static final ArrayList<File> collectFiles = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
             Document document = Jsoup.connect("https://skillbox-java.github.io/").get();
             Elements elements = document.select("h2");
-            elements.forEach(System.out::println);
-            parseLine(elements);
-            parseStations(elements);
+            //parseLine(elements);
+            //parseStations(elements);
+            fileReader();
+            fileParser(collectFiles);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -41,7 +53,6 @@ public class Main {
 
     private static void parseStations(Elements elements) {
         String[] data = elements.text().split("\\d+");
-        System.out.println(Arrays.toString(data));
         int stationNumber = 1;
         int check = 1;
         String stationName = "";
@@ -58,8 +69,7 @@ public class Main {
                     String[] ary = data[i].trim().split("\\s+");
                     if (ary.length == 3) {
                         stationName = ary[0].trim().replaceAll("\\.", "");
-                    }
-                    else if (ary.length > 3) {
+                    } else if (ary.length > 3) {
                         stationName = (ary[0] + " " + ary[1]).trim().replaceAll("\\.", "");
                     }
                     line.addStations(new Station(stationNumber, 0, stationName, line,
@@ -72,4 +82,35 @@ public class Main {
         }
         System.out.println(metro);
     }
+
+    private static ArrayList<File> fileReader() {
+        File dataFile = new File(file);
+        for (File f : dataFile.listFiles()) {
+            if (f.isDirectory()) {
+                for (File f1 : f.listFiles()) {
+                    if (f1.isDirectory()) {
+                        for (File file1 : f1.listFiles()) {
+                            if (file1.isFile()) {
+                                collectFiles.add(file1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(collectFiles);
+        return collectFiles;
+    }
+
+    private static void fileParser(ArrayList<File> collectFiles) {
+        for (File file1 : collectFiles) {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get(file1.getPath()));
+                System.out.println(lines);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
+
