5c8cd495f98f0a630cfd088fe58a0cadd3ae137e
Dmitriy Belkin <dimaalexxxxx13@gmail.com>
2022-11-15 02:30:11 +0300

    commit 2

--- SQLAndHibernate/averageOurchasePerMonth/src/main/java/Purchase.java
+++ SQLAndHibernate/averageOurchasePerMonth/src/main/java/Purchase.java
@@ -1,32 +1,54 @@
 import java.time.LocalDateTime;
 
-import java.time.LocalDateTime;
+public class Purchase {
 
-    public class Purchase {
+    private LocalDateTime subscriptionsDate;
 
-        private LocalDateTime subscriptionsDate;
+    private String courseName;
 
-        private String courseName;
+    private String studentName;
 
-        private String studentName;
+    private int price;
 
-        private int price;
+    public Purchase(LocalDateTime subscriptionsDate, String courseName) {
+        this.subscriptionsDate = subscriptionsDate;
+        this.courseName = courseName;
+    }
 
-        public Purchase (LocalDateTime subscriptionsDate, String courseName) {
-            this.subscriptionsDate = subscriptionsDate;
-            this.courseName = courseName;
-        }
-
-        public void setSubscriptionsDate(LocalDateTime subscriptionsDate) {
-
-        }
-
-        public LocalDateTime getSubscriptionsDate() {
-            return subscriptionsDate;
-        }
-        @Override
-        public String toString() {
-            return "" + subscriptionsDate;
-        }
+    public void setSubscriptionsDate(LocalDateTime subscriptionsDate) {
 
     }
+
+    public LocalDateTime getSubscriptionsDate() {
+        return subscriptionsDate;
+    }
+
+    public void setCourseName(String courseName) {
+
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setPrice(int price) {
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setStudentName(String studentName) {
+
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "" + subscriptionsDate;
+    }
+}
